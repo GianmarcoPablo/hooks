9306751b7b7f16a021342854bc36dc333bb8bb0c
Gian Marco Pablo <gianmarcopablo588@gmail.com>
2025-08-24 23:23:47 -0500

    hooks

---
+++ src/HooksApp.tsx
@@ -0,0 +1,9 @@
+
+
+export function HookApp() {
+   return (
+      <div className="bg-gradient">
+         <h1 className="text-5xl font-bold text-orange-800">hola mundo</h1>
+      </div>
+   );
+}
---
+++ src/useEffect/TrafficLightWithEffect.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+const colors = {
+   red: "bg-red-500 animate-pulse",
+   yellow: "bg-yellow-500 animate-pulse",
+   green: "bg-green-500 animate-pulse",
+}
+
+//type TraffictLightColor = "red" | "yellow" | "green"
+type TraffictLightColor = keyof typeof colors
+
+export const TrafficLightWithEffect = () => {
+
+   const [light, setLight] = useState<TraffictLightColor>("red");
+   const [countDown, setCountDown] = useState(5)
+
+   useEffect(() => {
+      console.log("creando uno nuevo")
+      if(countDown === 0) {
+         setCountDown(5)
+         if(light === "red"){
+            setLight("green")
+            return
+         }
+
+         if(light === "yellow"){
+            setLight("red")
+            return
+         }
+
+
+         if(light === "green"){
+            setLight("yellow")
+            return
+         }
+         return
+      }
+      const intervalId = setInterval(() => {
+         setCountDown((prev) => prev - 1)
+      }, 1000)
+
+      return () => {
+         console.log("limpiando interval")
+         clearInterval(intervalId)
+      }
+   }, [countDown,light])
+
+   return (
+      <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+         <div className="flex flex-col items-center space-y-8">
+            <h2 className="text-white text-3xl font-thin">Semaforo con useEffect</h2>
+            <h2 className="text-white text-xl">{countDown} </h2>
+            <div className={`w-32 h-32 ${light === "red" ? colors[light] : "bg-gray-500"} rounded-full`}></div>
+            <div className={`w-32 h-32 ${light === "yellow" ? colors[light] : "bg-gray-500"} rounded-full`}></div>
+            <div className={`w-32 h-32 ${light === "green" ? colors[light] : "bg-gray-500"} rounded-full`}></div>
+
+         </div>
+      </div>
+   );
+};
