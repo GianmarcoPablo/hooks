9c7b00c63485ba11209c2897ad6177b823dfb6fd
Gian Marco Pablo <gianmarcopablo588@gmail.com>
2025-08-25 22:59:46 -0500

    hooks pokemon

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect'
+import { PokemonPage } from './examples/PokemonPage'
+
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <TrafficLightWithEffect />
+      <PokemonPage />
    </StrictMode>,
 )
--- src/useEffect/TrafficLightWithEffect.tsx
+++ src/useEffect/TrafficLightWithEffect.tsx
@@ -11,45 +11,48 @@ type TraffictLightColor = keyof typeof colors
 
 export const TrafficLightWithEffect = () => {
 
-   const [light, setLight] = useState<TraffictLightColor>("red");
    const [countDown, setCountDown] = useState(5)
+   const [light, setLight] = useState<TraffictLightColor>("red");
 
    useEffect(() => {
-      console.log("creando uno nuevo")
-      if(countDown === 0) {
-         setCountDown(5)
-         if(light === "red"){
-            setLight("green")
-            return
-         }
-
-         if(light === "yellow"){
-            setLight("red")
-            return
-         }
-
-
-         if(light === "green"){
-            setLight("yellow")
-            return
-         }
-         return
-      }
+      if (countDown === 0) return
       const intervalId = setInterval(() => {
          setCountDown((prev) => prev - 1)
       }, 1000)
 
       return () => {
-         console.log("limpiando interval")
          clearInterval(intervalId)
       }
-   }, [countDown,light])
+   }, [countDown])
+
+
+   useEffect(() => {
+      if (countDown > 0) return
+      setCountDown(5)
+      if (light === "red") {
+         setLight("green")
+         return
+      }
+      if (light === "green") {
+         setLight("yellow")
+         return
+      }
+      if (light === "yellow") {
+         setLight("red")
+         return
+      }
+   }, [countDown, light])
 
    return (
       <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
          <div className="flex flex-col items-center space-y-8">
             <h2 className="text-white text-3xl font-thin">Semaforo con useEffect</h2>
             <h2 className="text-white text-xl">{countDown} </h2>
+            <div className="w-64 bg-gray-700 rounded-full h-2">
+               <div className="bg-blue-700 h-2 rounded-full transition-all duration-1000 ease-linear" style={{
+                  width: `${countDown / 5 * 100}%`
+               }}></div>
+            </div>
             <div className={`w-32 h-32 ${light === "red" ? colors[light] : "bg-gray-500"} rounded-full`}></div>
             <div className={`w-32 h-32 ${light === "yellow" ? colors[light] : "bg-gray-500"} rounded-full`}></div>
             <div className={`w-32 h-32 ${light === "green" ? colors[light] : "bg-gray-500"} rounded-full`}></div>
